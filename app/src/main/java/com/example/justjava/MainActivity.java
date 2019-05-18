099678a890b20b47d8d7aa22c132c798dd571c4d
Remon <remongerges94@gmail.com>
2019-05-18 16:21:00 +0200

    Add quantity picker

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -19,9 +19,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void SubmitOrder(View view) {
-        int quantity = 2;
+        int quantity = 5;
         display(quantity);
         displayPrice(quantity * 5);
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
     }
 
     /**
