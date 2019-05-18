91e37a7631617f7330d9b6b5b1d9ad7d23473f7d
Remon <remongerges94@gmail.com>
2019-05-18 17:39:19 +0200

    Fix: submitOrder

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -21,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void SubmitOrder(View view) {
-        int quantity = 5;
-        display(quantity);
         displayPrice(quantity * 5);
     }
 
