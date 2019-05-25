690404b5c8d813ec5a50f0a03fe9b5e453f25fd0
Remon <remongerges94@gmail.com>
2019-05-25 17:17:14 +0200

    Add comments and refactor code

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
+    private int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +24,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void SubmitOrder(View view) {
+
+        //get the views by their IDs
         CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
         CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
-
         EditText nameEditText = findViewById(R.id.name_edit_text);
+
+        //check if the customer checked any of the checkboxes
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
         boolean hasChocolate = chocolateCheckBox.isChecked();
+        //get the customer name
         String name = nameEditText.getText().toString();
 
+        //get the price of the coffee ordered
         int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        //get the order summary
         String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
 
+        //send the order via email using intent
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.order_summary_name) + name);
@@ -48,11 +56,14 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+
+        //the max quantity of coffees is 100
         if (quantity == 100) {
             Toast.makeText(getApplicationContext(), "You cannot have more than 100 coffees",
                     Toast.LENGTH_SHORT).show();
             return;
         }
+
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
@@ -61,11 +72,14 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+
+        // the min quantity of the coffee is 1 cup of coffee
         if (quantity == 1) {
             Toast.makeText(getApplicationContext(), "Cannot be less than 1 Coffee",
                     Toast.LENGTH_SHORT).show();
             return;
         }
+
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
@@ -74,8 +88,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void displayQuantity(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(number);
     }
 
     /**
@@ -84,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
      * @return total price
      */
     private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+
+        //price per cup
         int basePrice = 5;
         if (addWhippedCream) {
             basePrice += 1;
@@ -91,6 +107,8 @@ public class MainActivity extends AppCompatActivity {
         if (addChocolate) {
             basePrice += 2;
         }
+
+        //total price = price per cup x quantity
         return basePrice * quantity;
     }
 
